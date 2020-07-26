e74d953145829d9b5ffe7c31045a839e7a059afa
Juan Andres Avelino Ospina <javelino2311@gmail.com>
2020-07-26 00:12:56 -0500

    Profesor added

--- backend/models.py
+++ backend/models.py
@@ -3,4 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Profesor(models.Model):
-
+    nombre = models.CharField(max_length=60)
+    correo = models.EmailField()
+    celular = models.IntegerField()
